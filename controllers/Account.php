6660a47073d22d54a87efe1685281e634553828c
«Meastrix001» <Nroofthooft2012@gmail.com>
2021-11-05 13:53:13 +0200

    updated comments styling

--- controllers/Account.php
+++ controllers/Account.php
@@ -65,42 +65,6 @@ class AccountController extends BaseController {
         $this->viewParams['comments'] = Comments::getAllById($params[0]);
 
         echo "detail";
-
-        if(isset($_POST['create_post'])  ){
-            global $loggedIn_user;
-            $valid = true;
-            $post = new Posts();
-            $post->description = trim( $_POST['description'] );
-            $post->created_on = date("Y/m/d");
-            $post->users_id = $loggedIn_user->id;
-
-            //post
-            $uploads_dir = BASE_DIR . '/images/posts/';
-            $name = $_FILES['file']['name'];
-            if(isset($name) and !empty($name)){     
-                if(is_uploaded_file($_FILES['file']['tmp_name'])); {
-                    move_uploaded_file($_FILES['file']['tmp_name'], $uploads_dir.$name);
-                    $post->media = $name;
-                }
-            } else {
-                echo 'You should select a file to upload !!';
-            }
-
-            if( empty($post->description) || empty($post->media)) {
-                $valid = false;
-            }
-
-            if($valid) {
-                    $post_id = $post->createPost($post);
-                    echo "Succesvol $post_id aangemaakt";
-                    header('content:');
-            }
-            else {
-                //give feedback
-                echo 'FAIL';
-            }
-            
-        }
         $this->loadView();
 
     }
